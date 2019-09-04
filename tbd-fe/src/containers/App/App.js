e76a8ceebb22e9e14002a19978a8e89b4717e0af
DeMarcus Kirby <tkd024@gmail.com>
2019-09-04 14:25:02 -0600

    Add Form Component and Styling

--- tbd-fe/src/containers/App/App.js
+++ tbd-fe/src/containers/App/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import './App.scss';
+import Form from '../Form/Form.js';
 
 class App extends Component {
+
+  componentDidMount() {
+    console.log('Working')
+    fetch('https://radiant-peak-49102.herokuapp.com/api/v1/listings?keywords=developer&location=portland')
+    .then(data => data.json())
+    .then(data => console.log(data.data.attributes.return_all_listings.jobs))
+    .catch(error => console.log(error))
+  }
 
   render() {
     return (
       <div className="App">
-        <h1>hello</h1>
+        <h1>LinkedIn Clone</h1>
+        <Form />
       </div>
     )
   }
