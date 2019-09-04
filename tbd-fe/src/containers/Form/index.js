849faab93578a6670b9263795e30038099bc5179
Jacqueline Moore <jacquelinebmoore@gmail.com>
2019-09-04 17:47:57 -0600

    Import normalize.css and fiddle with Form style

---
+++ tbd-fe/src/containers/Form/index.js
@@ -0,0 +1,65 @@
+import React, {Component} from 'react';
+import './Form.scss'
+
+class Form extends Component {
+  constructor() {
+    super();
+    this.state = {
+      keywords: '',
+      location: '',
+      radius: '',
+      salary: ''
+    }
+  }
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+  }
+
+  render() {
+    const { keywords, location, radius, salary } = this.state;
+    return(
+      <form 
+        className="search-form"
+        onChange={this.handleChange}>
+        <label className="search-label">Job Title / Keywords
+          <input 
+            className="form-input keyword-input"
+            type="text"
+            name='keywords' 
+            value={keywords} />
+        </label>
+        <label className="search-label">Desired Salary
+          <input 
+            className="form-input salary-input"
+            type="text"
+            name='salary' 
+            value={salary} />
+        </label>
+        <label className="search-label">Location
+          <input 
+            className="form-input location-input"
+            type="text" 
+            name='location' 
+            value={location} />
+        </label>
+        <label className="search-label radius-label">Location Radius
+          <input
+            className="form-input radius-input"
+            type="number"  
+            name='radius' 
+            value={radius} />
+        </label>
+        <button className='form-btn' onClick={this.handleSubmit}>Submit</button>
+      </form>
+    )
+  }
+}
+
+export default Form;
