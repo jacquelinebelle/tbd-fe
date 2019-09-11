016e3a994ba337fd8212e68c9a798874c44f3cdc
DeMarcus Kirby <tkd024@gmail.com>
2019-09-10 20:22:02 -0600

    Add mock job and basic city data

---
+++ src/mockData/mockCities.js
@@ -0,0 +1,238 @@
+const mockCities = [
+  {
+    city: "Mesa, AZ",
+    mobile:
+      "https://d13k13wj6adfdf.cloudfront.net/urban_areas/phoenix-9fedc88f57.jpg",
+    web:
+      "https://d13k13wj6adfdf.cloudfront.net/urban_areas/phoenix_web-92224752db.jpg",
+    teleport_city_score: 56.66256756756756,
+    summary:
+      "<p>Phoenix, Arizona, is among the top cities with a <b>free business environment</b>. According to our city rankings, this is a good place to live with high ratings in <b>healthcare</b>, <b>leisure & culture</b> and <b>outdoors</b>. </p> <p>Phoenix is one of the top ten city matches for 0.2% of Teleport users.</p> ",
+    categories: [
+      {
+        color: "#f3c32c",
+        name: "Housing",
+        score_out_of_10: 6.5329999999999995
+      },
+      { color: "#f3d630", name: "Cost of Living", score_out_of_10: 6.208 },
+      {
+        color: "#f4eb33",
+        name: "Startups",
+        score_out_of_10: 6.508500000000001
+      },
+      { color: "#d2ed31", name: "Venture Capital", score_out_of_10: 4.558 },
+      {
+        color: "#7adc29",
+        name: "Travel Connectivity",
+        score_out_of_10: 3.3024999999999998
+      },
+      { color: "#36cc24", name: "Commute", score_out_of_10: 4.828500000000001 },
+      { color: "#19ad51", name: "Business Freedom", score_out_of_10: 8.671 },
+      { color: "#0d6999", name: "Safety", score_out_of_10: 2.5829999999999997 },
+      {
+        color: "#051fa5",
+        name: "Healthcare",
+        score_out_of_10: 8.567333333333334
+      },
+      {
+        color: "#150e78",
+        name: "Education",
+        score_out_of_10: 4.590999999999999
+      },
+      {
+        color: "#3d14a4",
+        name: "Environmental Quality",
+        score_out_of_10: 4.625249999999999
+      },
+      { color: "#5c14a1", name: "Economy", score_out_of_10: 6.5145 },
+      { color: "#88149f", name: "Taxation", score_out_of_10: 4.488 },
+      { color: "#b9117d", name: "Internet Access", score_out_of_10: 6.178 },
+      {
+        color: "#d10d54",
+        name: "Leisure & Culture",
+        score_out_of_10: 6.8100000000000005
+      },
+      {
+        color: "#e70c26",
+        name: "Tolerance",
+        score_out_of_10: 5.5649999999999995
+      },
+      { color: "#f1351b", name: "Outdoors", score_out_of_10: 6.7355 }
+    ]
+  },
+  {
+    city: "Durham, NC",
+    mobile:
+      "https://d13k13wj6adfdf.cloudfront.net/urban_areas/raleigh-e74249142f.jpg",
+    web:
+      "https://d13k13wj6adfdf.cloudfront.net/urban_areas/raleigh_web-2909c09abe.jpg",
+    teleport_city_score: 62.51567567567569,
+    summary:
+      "<p>Raleigh, North Carolina, is among the top citie…ten city matches for 3.4% of Teleport users.</p>",
+    categories: [
+      { color: "#f3c32c", name: "Housing", score_out_of_10: 6.128500000000001 },
+      {
+        color: "#f3d630",
+        name: "Cost of Living",
+        score_out_of_10: 5.610000000000001
+      },
+      { color: "#f4eb33", name: "Startups", score_out_of_10: 5.497 },
+      {
+        color: "#d2ed31",
+        name: "Venture Capital",
+        score_out_of_10: 5.157000000000002
+      },
+      {
+        color: "#7adc29",
+        name: "Travel Connectivity",
+        score_out_of_10: 1.7570000000000001
+      },
+      { color: "#36cc24", name: "Commute", score_out_of_10: 4.78425 },
+      { color: "#19ad51", name: "Business Freedom", score_out_of_10: 8.671 },
+      { color: "#0d6999", name: "Safety", score_out_of_10: 6.5225 },
+      {
+        color: "#051fa5",
+        name: "Healthcare",
+        score_out_of_10: 8.966666666666667
+      },
+      { color: "#150e78", name: "Education", score_out_of_10: 7.1165 },
+      {
+        color: "#3d14a4",
+        name: "Environmental Quality",
+        score_out_of_10: 8.583750000000002
+      },
+      { color: "#5c14a1", name: "Economy", score_out_of_10: 6.5145 },
+      { color: "#88149f", name: "Taxation", score_out_of_10: 4.062 },
+      { color: "#b9117d", name: "Internet Access", score_out_of_10: 7.9515 },
+      {
+        color: "#d10d54",
+        name: "Leisure & Culture",
+        score_out_of_10: 6.6129999999999995
+      },
+      {
+        color: "#e70c26",
+        name: "Tolerance",
+        score_out_of_10: 7.562000000000001
+      },
+      {
+        color: "#f1351b",
+        name: "Outdoors",
+        score_out_of_10: 3.3905000000000003
+      }
+    ]
+  },
+  {
+    city: "Minneapolis, MN",
+    mobile:
+      "https://d13k13wj6adfdf.cloudfront.net/urban_areas/minneapolis-saint-paul-0167dd447d.jpg",
+    web:
+      "https://d13k13wj6adfdf.cloudfront.net/urban_areas/minneapolis-saint-paul_web-16dbfd4210.jpg",
+    teleport_city_score: 61.163648648648646,
+    summary:
+      "<p>Minneapolis-Saint Paul, Minnesota, is among the…ten city matches for 4.5% of Teleport users.</p>↵",
+    categories: [
+      { color: "#f3c32c", name: "Housing", score_out_of_10: 4.738500000000001 },
+      {
+        color: "#f3d630",
+        name: "Cost of Living",
+        score_out_of_10: 5.042000000000001
+      },
+      {
+        color: "#f4eb33",
+        name: "Startups",
+        score_out_of_10: 6.5264999999999995
+      },
+      {
+        color: "#d2ed31",
+        name: "Venture Capital",
+        score_out_of_10: 5.5230000000000015
+      },
+      { color: "#7adc29", name: "Travel Connectivity", score_out_of_10: 3.544 },
+      { color: "#36cc24", name: "Commute", score_out_of_10: 4.924999999999999 },
+      { color: "#19ad51", name: "Business Freedom", score_out_of_10: 8.671 },
+      { color: "#0d6999", name: "Safety", score_out_of_10: 5.085000000000001 },
+      { color: "#051fa5", name: "Healthcare", score_out_of_10: 8.972 },
+      { color: "#150e78", name: "Education", score_out_of_10: 5.2745 },
+      {
+        color: "#3d14a4",
+        name: "Environmental Quality",
+        score_out_of_10: 8.362
+      },
+      { color: "#5c14a1", name: "Economy", score_out_of_10: 6.5145 },
+      { color: "#88149f", name: "Taxation", score_out_of_10: 4.062 },
+      {
+        color: "#b9117d",
+        name: "Internet Access",
+        score_out_of_10: 6.258499999999999
+      },
+      { color: "#d10d54", name: "Leisure & Culture", score_out_of_10: 6.8505 },
+      { color: "#e70c26", name: "Tolerance", score_out_of_10: 7.288 },
+      { color: "#f1351b", name: "Outdoors", score_out_of_10: 5.095999999999999 }
+    ]
+  },
+  {
+    city: "Calabasas, CA",
+    mobile:
+      "https://d13k13wj6adfdf.cloudfront.net/urban_areas/los-angeles-f5b60deb04.jpg",
+    web:
+      "https://d13k13wj6adfdf.cloudfront.net/urban_areas/los-angeles_web-2eedb2a83e.jpg",
+    teleport_city_score: 63.426351351351364,
+    summary:
+      "<p>↵    Los Angeles is a sprawling city of 500 squ…en city matches for 11.5% of Teleport users.</p>↵",
+    categories: [
+      {
+        color: "#f3c32c",
+        name: "Housing",
+        score_out_of_10: 1.5274999999999999
+      },
+      {
+        color: "#f3d630",
+        name: "Cost of Living",
+        score_out_of_10: 4.556000000000001
+      },
+      { color: "#f4eb33", name: "Startups", score_out_of_10: 10 },
+      { color: "#d2ed31", name: "Venture Capital", score_out_of_10: 10 },
+      {
+        color: "#7adc29",
+        name: "Travel Connectivity",
+        score_out_of_10: 3.9585000000000004
+      },
+      {
+        color: "#36cc24",
+        name: "Commute",
+        score_out_of_10: 3.6770000000000005
+      },
+      {
+        color: "#19ad51",
+        name: "Business Freedom",
+        score_out_of_10: 8.574666666666667
+      },
+      { color: "#0d6999", name: "Safety", score_out_of_10: 5.705 },
+      {
+        color: "#051fa5",
+        name: "Healthcare",
+        score_out_of_10: 8.439666666666666
+      },
+      { color: "#150e78", name: "Education", score_out_of_10: 8.6245 },
+      {
+        color: "#3d14a4",
+        name: "Environmental Quality",
+        score_out_of_10: 4.7315000000000005
+      },
+      { color: "#5c14a1", name: "Economy", score_out_of_10: 6.5145 },
+      { color: "#88149f", name: "Taxation", score_out_of_10: 4.7675 },
+      {
+        color: "#b9117d",
+        name: "Internet Access",
+        score_out_of_10: 5.496500000000001
+      },
+      { color: "#d10d54", name: "Leisure & Culture", score_out_of_10: 9.196 },
+      {
+        color: "#e70c26",
+        name: "Tolerance",
+        score_out_of_10: 6.959999999999999
+      },
+      { color: "#f1351b", name: "Outdoors", score_out_of_10: 6.747 }
+    ]
+  }
+];
