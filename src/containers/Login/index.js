077fc6ed83e6b662cdf2c5a03f38df51ffe20646
Patrick Goulding <patrickgoulding@gmail.com>
2019-09-11 09:31:37 -0600

    WIP login component, on back burner till other features done

---
+++ src/containers/Login/index.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+export class index extends Component {
+  constructor() {
+    super()
+    this.state={
+      email:'',
+      password:''
+    }
+  }
+
+  login = () => {
+    console.log(this.state)
+  }
+
+  handleChange =(e) => {
+    this.setState={[e.etarget.name]: e.target.value}
+  }
+
+
+  render() {
+    return (
+      <div>
+        <form>
+          <input 
+            placeholder="Email..."
+            value={this.state.email}
+            onChange={this.handleChange}
+          />
+          <input 
+            placeholder="Password"
+            type="password"
+            onChange={this.handleChange}
+          />
+          <button onClick={()=> this.login()}>
+
+          </button>
+        </form>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state) => ({
+  
+})
+
+const mapDispatchToProps =(dispatch) => ({
+  
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(index)
