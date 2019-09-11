886475c079ab350d45257d34041452e32f09072c
Patrick Goulding <patrickgoulding@gmail.com>
2019-09-11 09:31:00 -0600

    add reducers for log in/log out

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import { jobsReducer, loading, error } from './jobsReducer';
 import { cityReducer } from './cityReducer';
+import { userReducer } from './userReducer';
 
 export const rootReducer = combineReducers({
   jobs: jobsReducer,
   cities: cityReducer,
   loading,
-  error
+  error,
+  login: userReducer
 });
