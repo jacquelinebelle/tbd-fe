d19ce960f20712b422c1ec5194427a921f725ccb
Patrick Goulding <patrickgoulding@gmail.com>
2019-09-11 20:47:57 -0600

    fix linting errors

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Switch, Link } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import ResultsPage from '../ResultsPage';
 import './App.scss';
 import Form from '../Form/';
